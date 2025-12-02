eb2934c91c057741fc82f70ef4b19298a23b3406
vijaysatheesh <vijaysatheesh2024@gmail.com>
2025-12-02 22:16:41 +0530

    Blink using timer

--- blink.c
+++ blink.c
@@ -1,24 +1,39 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdbool.h>
+#include <avr/interrupt.h>
 
-#define BLINK_DELAY 500
+#define CLOCK_RATE 16000000
+#define PRESCALER 10
+#define COUNTER_VALUE (CLOCK_RATE >> PRESCALER) // How many clock tics to wait to get an interrupt
+
+ISR(TIMER1_COMPA_vect){
+  PORTB ^= _BV(PORTB5);
+}
+
 
 int main()
 {
-  //DDRB |= 0b00100000;S
+
   DDRB |= _BV(PORT5);
+
+  OCR1A = COUNTER_VALUE; // Output compare register for timer 1
+
+  TCCR1A = 0x00;          // Timer counter configuration register for timer 1
+  TCCR1B |= (1 << WGM12); // for CTC mode (clear timer on compare).
+                          // each clocktick the value of timer is compared to OCR1A register.
+                          // if matches clear timer and give interrupt.
+  TCCR1B |= (1 << CS10) | (1 << CS12); // set the prescaler to divide values by 1024.
+                                       // sets the CS10th and CS12th bits 1.
+
+  TIMSK1 |= (1<< OCIE1A) ; // Timwe inturrupt mask register. 
+                      //sets the OCIE (output compare interrupt enable)
+  
+  sei(); // enables interrupts
   while (true)
   {
-    //  PORTB |= _BV(PORT5);
-    //  _delay_ms(BLINK_DELAY);
 
-    //  PORTB &= ~_BV(PORT5);
-    //  _delay_ms(BLINK_DELAY);
-
-    PORTB ^= _BV(PORT5);
-    _delay_ms(BLINK_DELAY);  
   }
-  
+
   return 0;
 }
